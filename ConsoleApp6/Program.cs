56a66bf935dd59b1a7c36acd27aec760ca75614f
psi9 <semyon.pavlenko@gmail.com>
2021-10-24 23:59:54 +1000

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp6 {
+
+
+    class Program
+        
+    {
+        delegate void Delegator();
+        static void Privet()
+        {
+            Console.WriteLine("111");
+        }
+        public class Point
+        {
+            private double x;
+            private double y;
+
+            public double X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+
+            public double Y
+            {
+                get { return y; }
+
+                set { y = value; }
+            }
+
+
+ 
+        }
+
+
+        static void Main(string[] args)
+        {
+            Delegator delegator = Privet;
+            Delegator delegator2 = () => Console.WriteLine("222");
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ events/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace events
+{
+    class Program
+    {
+        public delegate void Delegator();
+        class Class
+        {
+            int pole;
+            public int Pole { 
+                get { return pole;  } 
+                set {
+                    pole = value;
+                    onChange();
+                } 
+            }
+            public event Delegator Event;
+            void onChange()
+            {
+                if (Event != null)
+                {
+                    Event();
+                }
+            }
+        }
+        static void Primer ()
+        {
+            Console.WriteLine("event raised");
+        }
+
+        static void Main(string[] args)
+        {
+            var cool = new Class();
+            cool.Event += Primer;
+            cool.Pole = 10;
+            cool.Pole = 11;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
