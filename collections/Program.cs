e8e70f5ae39b065ba1c1ddda2f35e70de79cd6aa
psi9 <semyon.pavlenko@gmail.com>
2021-10-26 00:45:33 +1000

    .

---
+++ collections/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace collections
+{
+    class Collection : IEnumerable, IEnumerator
+    {
+        public int[] array = { 1, 2, 3 };
+
+        int index = -1;
+
+        public object Current => array[index];
+
+        public bool MoveNext()
+        {
+            if (index < array.Length - 1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Collection();
+            foreach (var i in x)
+                Console.WriteLine(i);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ yeilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace yeilder
+{
+    class Collection
+    {
+        public static IEnumerable Yield(int a, int b)
+        {
+            for (var x = 0; x < b; x++)
+                yield return a + b + x;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var i in Collection.Yield(2, 10))
+                Console.WriteLine(i);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
